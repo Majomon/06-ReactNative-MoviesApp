580cb3e1ec3fffda18d191f8cc7c6ce6cd9cdd83
Mauri <mauri.monzon91@gmail.com>
2024-06-05 15:18:26 -0300

    Navegacion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'react-hooks/exhaustive-deps': 'off', // Desactiva la advertencia sobre dependencias de useEffect
+    'react-native/no-inline-styles': 'off', // Desactiva la advertencia sobre estilos en línea
+    'react/no-unstable-nested-components': 'off', // Desactiva la advertencia sobre componentes anidados inestables
+  },
 };
