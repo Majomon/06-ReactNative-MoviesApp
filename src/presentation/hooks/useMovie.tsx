4f7e52c4c41ab0c058c7c1d0e564cc654bbfe4e5
Mauri <mauri.monzon91@gmail.com>
2024-06-13 10:36:02 -0300

    Lista de actores

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -2,10 +2,12 @@ import {useEffect, useState} from 'react';
 import * as UseCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 import {FullMovie} from '../../core/entities/movie.entity';
+import {Cast} from '../../core/entities/cast.entity';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect(() => {
     loadMovie();
@@ -14,19 +16,25 @@ export const useMovie = (movieId: number) => {
   const loadMovie = async () => {
     setIsLoading(true);
 
-    const fullMovie = await UseCases.getMovieByIdUseCase(
+    const fullMoviePromise = UseCases.getMovieByIdUseCase(
       movieDBFetcher,
       movieId,
     );
+    const castPromise = UseCases.getMovieCastUseCase(movieDBFetcher, movieId);
+
+    const [fullMovie, castMovie] = await Promise.all([
+      fullMoviePromise,
+      castPromise,
+    ]);
 
     setMovie(fullMovie);
+    setCast(castMovie);
     setIsLoading(false);
-
-    console.log(fullMovie);
   };
 
   return {
     isLoading,
     movie,
+    cast,
   };
 };
